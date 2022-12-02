97a86b0d45616b2297b9bd800efbd259bf3cf100
yeomsungpil <present7256@gmail.com>
2022-12-02 10:12:08 +0900

    wowo
    wowo pratice

---
+++ wowo/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  wowo
+//
+//  Created by 염성필 on 2022/12/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wowo/wowoApp.swift
@@ -0,0 +1,17 @@
+//
+//  wowoApp.swift
+//  wowo
+//
+//  Created by 염성필 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct wowoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
