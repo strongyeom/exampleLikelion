be2fb9572c9fdc4e4cc7eea9eb84609f4ea80a84
yeomsungpil <present7256@gmail.com>
2022-12-02 10:33:18 +0900

    Second Commit

--- wowo/ContentView.swift
+++ wowo/ContentView.swift
@@ -14,8 +14,6 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
-                .font(.largeTitle)
-                .fontWeight(.bold)
         }
         .padding()
     }
