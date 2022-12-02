b5343b340655b0a09ffdc574ae2bad2fb72dcc43
yeomsungpil <present7256@gmail.com>
2022-12-02 10:23:27 +0900

    message-1

--- wowo/ContentView.swift
+++ wowo/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
                 .font(.largeTitle)
+                .fontWeight(.bold)
         }
         .padding()
     }
